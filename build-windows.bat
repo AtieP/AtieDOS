3d4f29feaa62484a37fd820099fcbf6967f70853
AtieP <62116490+AtieP@users.noreply.github.com>
2020-03-14 11:27:50 +0100

    Delete build-windows.bat

--- build-windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set /p nasmdir="Type here the directory that has NASM (please use double quotes) If you already have it on your PATH put nasm: "
-if %nasmdir% == nasm goto yes
-set path=%path%;%nasmdir%
-:yes
-
-nasm -f bin -o atiedos-2-10.iso bootloader.asm
