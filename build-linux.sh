692f192e435b25f107a56a1da58207473bf7ce0e
AtieP <62116490+AtieP@users.noreply.github.com>
2020-03-14 11:27:36 +0100

    Delete build-linux.sh

--- build-linux.sh
+++
@@ -1,4 +0,0 @@
-# you will need nasm to compile this
-# and you need to put in the command line processor before compiling
-# "chmod u+w build-linux.sh".
-nasm -f bin -o atiedos-2-10.iso bootloader.asm
